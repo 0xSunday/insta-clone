dffad677f718f155f41bcc0a62ff6a5c8a4c99c9
0xSunil <sunday7637@gmail.com>
2023-03-06 20:30:08 +0530

    fix bug 3

--- components/middle/Post.js
+++ components/middle/Post.js
@@ -7,11 +7,77 @@ import { FiSend } from "react-icons/fi";
 import { BiBookmark } from "react-icons/bi";
 import { BsEmojiSmile } from "react-icons/bs";
 import { useState, useEffect } from "react";
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
 import { db } from "@/firebase";
 const Post = () => {
   const [posts, setPosts] = useState([]);
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  const [likes, setLikes] = useState([]);
+  const [hasLiked, setHasLiked] = useState(false);
 
+  // useEffect(
+  //   () =>
+  //     onSnapshot(
+  //       query(
+  //         collection(db, "posts", posts.id , "comments"),
+  //         orderBy("timestamp", "desc")
+  //       ),
+  //       (snapshot) => setComments(snapshot.docs)
+  //     ),
+  //   [db]
+  // );
+
+  // useEffect(
+  //   () =>
+  //     onSnapshot(collection(db, "posts", posts.id, "likes"), (snapshot) =>
+  //       setLikes(snapshot.docs)
+  //     ),
+  //   [db, posts.id]
+  // );
+
+  // useEffect(
+  //   () =>
+  //     setHasLiked(
+  //       likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+  //     ),
+  //   [likes]
+  // );
+
+  // const likePost = async () => {
+  //   if (hasLiked) {
+  //     await deleteDoc(doc(db, "posts", posts.id, "likes", session.user.uid));
+  //   } else {
+  //     await setDoc(doc(db, "posts", posts.id, "likes", session.user.uid), {
+  //       username: session.user.username,
+  //     });
+  //   }
+  // };
+
+  // const sendComment = async (e) => {
+  //   e.preventDefault();
+  //   const commentToSend = comment;
+  //   setComment("");
+  //   await addDoc(collection(db, "posts", posts.id, "comments"), {
+  //     comment: commentToSend,
+  //     username: session.user.username,
+  //     userImage: session.user.image,
+  //     timestamp: serverTimestamp(),
+  //   });
+  // };
+
+  // // post
+  
   useEffect(
     () =>
       onSnapshot(
@@ -21,18 +87,12 @@ const Post = () => {
     [db]
   );
   console.log(posts);
-  const commentRef = useRef();
-  const commentHandler = (e) => {
-    e.preventDefault();
-    const comment = commentRef.current.value;
-    // console.log(comment);
-  };
 
   return (
     <section className="  bg-white pb-11 ">
       {posts.map((p, i) => (
         <div
-      // sunil
+          // sunil
           key={i}
           className={` ${
             i === posts.length - posts.length ? "mt-0 mb-5" : "my-5"
@@ -46,7 +106,11 @@ const Post = () => {
                 className="h-10 p-[1px] w-full rounded-full border-2 border-red-600 "
               />
               <h3 className="font-semibold">{p.data().userName} </h3>
-              <p className="text-gray-700">30 </p>
+
+              <Moment fromNow className="pr-5 text-gray-700">
+                {p.data().timestamp?.toDate()}
+              </Moment>
+              {/* <p className="text-gray-700">30 </p> */}
             </div>
             <FiMoreHorizontal />
           </div>
@@ -81,27 +145,54 @@ const Post = () => {
               />
             </div>
           </div>
-          <div className="px-3 font-semibold">15 likes</div>
+          <div className="px-3 font-semibold">{likes.length} likes</div>
           <div className="px-3 leading-5 pr-9">
             <span className="font-bold  ">{p.data().userName}</span>{" "}
             {p.data().caption}
           </div>
 
-          <form
-            onSubmit={commentHandler}
-            className="px-3 flex items-center justify-between"
-          >
+          {/* {comments.length > 0 && (
+            <div className="ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
+              {comments.map((comment) => (
+                <div
+                  key={comment.id}
+                  className="flex items-center space-x-2 mb-3"
+                >
+                  <img
+                    className="h-7 rounded-full"
+                    src={comment.data().userImage}
+                    alt=""
+                  />
+                  <p className="text-sm flex-1">
+                    <span className="font-bold">{comment.data().username}</span>{" "}
+                    {comment.data().comment}
+                  </p>
+                  <Moment fromNow className="pr-5 text-xs">
+                    {comment.data().timestamp?.toDate()}
+                  </Moment>
+                </div>
+              ))}
+            </div>
+          )} */}
+
+          <form className="px-3 flex items-center justify-between">
             <div className="flex justify-center items-center gap-2">
               <BsEmojiSmile />
               <input
                 type="text"
                 placeholder="Add a comment..."
+                // value={comment}
+                // onChange={(e) => setComment(e.target.value)}
                 className="border-none flex-1 focous:ring-0 outline-none"
-                ref={commentRef}
               />
             </div>
 
-            <button className="border-none text-blue-600 lg:hover:scale-125 transition-transform duration-300 ease-out">
+            <button
+              disabled={!comment.trim()}
+              // onClick={sendComment}
+              type="submit"
+              className="border-none text-blue-600 lg:hover:scale-125 transition-transform duration-300 ease-out"
+            >
               post
             </button>
           </form>
